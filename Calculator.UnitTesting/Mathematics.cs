6bc09fe9517c91a8aeb3badb3c28bc359ab67c91
berdikulov-571 <mygithub571@gmail.com>
2023-12-12 21:03:28 +0500

    Mathematics

---
+++ Calculator.UnitTesting/Mathematics.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator.UnitTesting
+{
+    public static class Mathematics
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Dividge(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Value cannot be zero");
+            }
+
+            return a / b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
